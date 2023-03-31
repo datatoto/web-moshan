fb3e3d1721d671c9ebb57360a7830935b5147142
BarbarossaWang <jiewangcn@outlook.com>
2023-03-31 18:26:06 +0800

    init repo

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import "./App.css";
+
+import { Canvas } from "@react-three/fiber";
+import { Environment, OrbitControls } from "@react-three/drei";
+import { Suspense } from "react";
+import { Model } from "./Ground";
+
+function App() {
+  return (
+    <div className="App">
+      <Canvas>
+        <Suspense fallback={null}>
+          <Model />
+          <OrbitControls />
+          <Environment preset="sunset" background />
+        </Suspense>
+      </Canvas>
+    </div>
+  );
+}
+
+export default App;
